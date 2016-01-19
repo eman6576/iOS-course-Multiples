c013eb5b005afa42679d2315a1d75a290545094f
Emanuel  Guerrero <Emanuel@Emanuels-MacBook-Pro.local>
2016-01-18 21:27:49 -0500

    Declared and implemented the method to handle the event of the user tapping on the play button.

--- Multiples/ViewController.swift
+++ Multiples/ViewController.swift
@@ -19,8 +19,28 @@ class ViewController: UIViewController {
     //Variables
     let numberOfIterations = 13
     var currentIteration = 0
-    let baseNumber = 0
+    var baseNumber = 0
     var previousMultiple = 0
     var newMultiple = 0
+    
+    @IBAction func playButtonPressed(sender: UIButton!) {
+        
+        if numberTextField.text != nil && numberTextField.text != "" {
+            
+            baseNumber = Int(numberTextField.text!)!
+            startGame()
+        }
+    }
+    
+    func startGame() {
+        
+        numberTextField.hidden = true
+        playButton.hidden = true
+        
+        multiplesLabel.hidden = false
+        addCoinButton.hidden = false
+        
+        multiplesLabel.text = "Press the coin to add!!!"
+    }
 }
 
