be57e64b7e5578e96555b29e8338bc4127c35bb5
Emanuel  Guerrero <Emanuel@Emanuels-MacBook-Pro.local>
2016-01-18 20:45:11 -0500

    Declared the outlets for the controls of the user interface.

--- Multiples/ViewController.swift
+++ Multiples/ViewController.swift
@@ -10,16 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    //Outlets
+    @IBOutlet weak var numberTextField: UITextField!
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var addCoinButton: UIButton!
+    @IBOutlet weak var multiplesLabel: UILabel!
 }
 
